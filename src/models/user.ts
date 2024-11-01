0ab39bb25a148b965ff779d3888630f21c07e82e
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-01 23:41:50 +0600

    feat: user model schema define done

---
+++ src/models/user.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: [3, "Name must be at least 3 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/, "Enter a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters"],
+  },
+  role: {
+    type: String,
+    enum: ["ADMIN", "USER"],
+    default: "USER",
+  },
+});
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
