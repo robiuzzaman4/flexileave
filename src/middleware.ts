5a8c998143fccd34f12f1342b360fa9d43a81317
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-03 00:03:02 +0600

    feat: complete middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,42 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
+import {
+  apiAuthPrefix,
+  authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+} from "@/routes";
 
 const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
-  console.log("pathname: ", req.nextUrl.pathname);
+export default auth(async (req) => {
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
-  console.log("isLoggedIn", isLoggedIn);
+
+  const isApiAuthRoute = nextUrl?.pathname?.startsWith(apiAuthPrefix);
+  const isAuthRoute = authRoutes.includes(nextUrl?.pathname);
+  const isPublicRoute = publicRoutes.includes(nextUrl?.pathname);
+
+  // if "/api/auth/*" then allow user to access ["IMPORTANT" order - 1]
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  // if "/login" or "/register" then redirect to login page if user is logged in ["IMPORTANT" order - 2]
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  // if not logged in and not a public route then redirect to login page ["IMPORTANT" order - 3]
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/login", nextUrl));
+  }
 });
 
+// route mathcher
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
