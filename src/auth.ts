476c8f3016996124205044569d29b706c055c850
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-02 23:31:26 +0600

    feat: separate auth and auth config file

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,8 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import authConfig from "@/auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    GitHub({
-      clientId: process.env.AUTH_GITHUB_ID,
-      clientSecret: process.env.AUTH_GITHUB_SECRET,
-    }),
-  ],
+  session: { strategy: "jwt" },
   secret: process.env.AUTH_SECRET,
+  ...authConfig,
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,13 @@
-export { auth as middleware } from "@/auth";
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  console.log("pathname: ", req.nextUrl.pathname);
+  const isLoggedIn = !!req.auth;
+  console.log("isLoggedIn", isLoggedIn);
+});
 
 export const config = {
   matcher: [
