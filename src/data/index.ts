24f6be0db9e8cb0bbee04963752b54eb623feddc
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-03 21:32:07 +0600

    feat: create get all users lib function

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import User from "@/models/user";
 
 export async function getUserByEmail(email: string) {
   const user = await User?.findOne({ email });
   return user;
+}
+
+export async function getAllUsers() {
+  try {
+    const users = await User.find({});
+    return {
+      success: true,
+      message: "Get all users fetched successfully",
+      data: users,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error?.message ?? "Something went wrong",
+    };
+  }
 }
