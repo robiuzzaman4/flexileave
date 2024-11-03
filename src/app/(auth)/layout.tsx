081c97a8ccdabef8065db4fdff6ea615ef857a74
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-03 14:49:05 +0600

    feat: update theme

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="min-h-[calc(100vh-56px)] w-full mt-14 flex items-center justify-center">
+    <div className="min-h-[calc(100vh-56px)] w-full mt-14 flex items-center justify-center px-4">
       {children}
     </div>
   );
