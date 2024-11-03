85736e62c7e36d919c68a9bc6df7c74dc23d01fe
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-03 23:15:07 +0600

    feat: add theme button with system theme dropdown

--- src/components/block/navbar.tsx
+++ src/components/block/navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { Button } from "@/components/ui/button";
-import ThemeToggle from "@/components/theme/theme-toggle";
+import ThemeBtn from "@/components/theme/theme-button";
 import { auth } from "@/auth";
 
 const Navbar = async () => {
@@ -25,7 +25,7 @@ const Navbar = async () => {
             </Button>
           )}
 
-          <ThemeToggle />
+          <ThemeBtn />
         </div>
       </div>
     </nav>
