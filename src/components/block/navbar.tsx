3c73d90e5c271a2db7cc372a57889549dc784b0d
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-04 00:03:14 +0600

    feat: update navbar

--- src/components/block/navbar.tsx
+++ src/components/block/navbar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import ThemeToggle from "@/components/theme/theme-toggle";
 import { auth } from "@/auth";
+import { ShieldHalf } from "lucide-react";
 
 const Navbar = async () => {
   const session = await auth();
@@ -10,8 +11,11 @@ const Navbar = async () => {
   return (
     <nav className="bg-secondary fixed top-0 w-full h-14 flex items-center border-b">
       <div className="w-full h-full max-w-screen-md mx-auto px-4 flex items-center justify-between">
-        <Link href="/" className="text-xl font-medium tracking-tighter">
-          Flexileave
+        <Link
+          href="/"
+          className="text-xl font-medium tracking-tighter flex items-center gap-1"
+        >
+          <ShieldHalf size={20} className="text-primary" /> <p>Auth</p>
         </Link>
 
         <div className="flex items-center gap-3">
